83fbe3046da7b78588aea6bdcea46ab129688f81
Perchzone <98227857+Perchzone@users.noreply.github.com>
2025-10-08 13:39:46 +0700

    about

--- modules/about.js
+++ modules/about.js
@@ -12,6 +12,9 @@ class AboutModule {
                 <h2>💎 FUEL THE REVOLUTIONARY EXPERIENCE</h2>
                 <p>It all began with a passion for DJing, gradually accumulating new skills, knowledge, and life experiences along the way.</p>
                 
+<p> DJ Perch has been digging in the crates and spinning vinyl since way back in 2005. His weapon of choice? A killer mix of funk, disco, soul, hip-hop, and trippy experimental electronics.<p/>
+<p>In 2022, he caught a serious analog synth fever. Now, he's a complete Terminator on the decks, mixing raw turntablism techniques, with deep, modular soundscapes. Don't sleep on this.<p/>
+
                 <p>Today, my work represents the culmination of my journey—everything I've absorbed, processed, and mixed with my own hands and mind.</p>
                 
                 <p>Constantly revolutionizing approaches and perspectives through sound and performance.</p>
